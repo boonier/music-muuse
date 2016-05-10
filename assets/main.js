6398499e2dc04d48bc0c801cbdf7683d028c3327
Si Mills <si@sjm-create.co.uk>
2016-05-11 00:11:14 +0100

    Make key grid

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,13 @@
+var keySize = offset = 30, // in px
+	gridSize = 576,
+	noteScale = [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
+	numOctaves = 6,
+	totalNotes = noteScale.length * numOctaves,
+	noteW = gridSize / totalNotes,
+	spacer = 0;
+
 function setup() {
-
-	var keySize = offset = 30, // in px
-		gridSize = 576;
-
-	var noteScale = [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
-		numOctaves = 6,
-		totalNotes = noteScale.length * numOctaves,
-		noteW = gridSize / totalNotes,
-		spacer = 0;
-
-	// background('#ccc');
 	createCanvas(gridSize + (offset * 2), gridSize + (offset * 2)); // 576px grid actual
-	stroke('#eee');
-	rect(offset, offset, gridSize - 1, gridSize - 1);
-
 	for (let i = 0; i < totalNotes; i++) {
 		if (noteScale[i % noteScale.length] == 1) {
 			fill('#333');
@@ -27,10 +21,20 @@ function setup() {
 		rect(width-keySize, spacer + offset, keySize, noteW); //right
 		spacer += noteW;
 	}
+	spacer = 0;
 }
 
 function draw() {
-	// background('rgba(0,0,0,0.1)');
-	// fill('#000')
-	// rect(mouseX, mouseY, 20, 20);
+	for (var i = 0; i < totalNotes; i++) {
+		if (noteScale[i % noteScale.length] == 1) {
+			fill('#f2f2f2');
+		} else {
+			noFill();
+		}
+		noStroke();
+		rect(spacer + offset, offset, noteW, gridSize); // hor
+		rect(offset, spacer + offset, gridSize, noteW); // vert
+		spacer += noteW;
+	}
+	spacer = 0;
 }
